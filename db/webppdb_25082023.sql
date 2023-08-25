525959fdaa40bf92939dc06e490d4d5c18057041
junizapermatanugraheni <junizanugraheni@gmail.com>
2023-08-25 07:34:04 +0700

    db

---
+++ db/webppdb_25082023.sql
@@ -0,0 +1,324 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 25 Agu 2023 pada 02.33
+-- Versi server: 10.4.25-MariaDB
+-- Versi PHP: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `webppdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `failed_jobs`
+--
+
+CREATE TABLE `failed_jobs` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `uuid` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2014_10_12_000000_create_users_table', 1),
+(2, '2014_10_12_100000_create_password_reset_tokens_table', 1),
+(3, '2019_08_19_000000_create_failed_jobs_table', 1),
+(4, '2019_12_14_000001_create_personal_access_tokens_table', 1),
+(5, '2023_08_14_135123_create_form_identitas_table', 2),
+(6, '2023_08_14_135707_create_form_nilais_table', 2),
+(7, '2023_08_14_135718_create_form_jurusans_table', 2),
+(8, '2023_08_14_135730_create_form_dokumens_table', 2),
+(9, '2023_08_21_135119_create_m_jurusans_table', 3),
+(10, '2023_08_22_142550_create_model_informasis_table', 4),
+(11, '2023_08_22_164839_create_users_table', 5),
+(12, '2023_08_23_142246_create_model_form_pendaftarans_table', 6);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `model_form_pendaftarans`
+--
+
+CREATE TABLE `model_form_pendaftarans` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `nopendaftaran` varchar(15) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nmlengkap` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nik` varchar(16) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nisn` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `alamat` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `notelp_siswa` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tmplahir` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tgllahir` date NOT NULL,
+  `jnskelamin` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `agama` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `asal_sekolah` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `alamat_sekolah` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nmortu` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `alamat_ortu` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `pekerjaan_ortu` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `notlp_ortu` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nilai_bindo` int(11) NOT NULL,
+  `nilai_binggris` int(11) NOT NULL,
+  `nilai_mtk` int(11) NOT NULL,
+  `nilai_ipa` int(11) NOT NULL,
+  `doc_nilaiujian` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `idjurusan` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `doc_ketlulus` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `doc_foto` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `doc_lainnya` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `model_form_pendaftarans`
+--
+
+INSERT INTO `model_form_pendaftarans` (`id`, `nopendaftaran`, `nmlengkap`, `nik`, `nisn`, `alamat`, `notelp_siswa`, `tmplahir`, `tgllahir`, `jnskelamin`, `agama`, `asal_sekolah`, `alamat_sekolah`, `nmortu`, `alamat_ortu`, `pekerjaan_ortu`, `notlp_ortu`, `nilai_bindo`, `nilai_binggris`, `nilai_mtk`, `nilai_ipa`, `doc_nilaiujian`, `idjurusan`, `doc_ketlulus`, `doc_foto`, `doc_lainnya`, `created_at`, `updated_at`) VALUES
+(1, '0', 'saya', '1234567890123456', '112233', 'Bansari Kepek wonosari', '083842161224', 'gunungkidul', '2007-12-12', 'laki-laki', 'islam', 'smp 4 wno', 'Bansari Kepek wonosari', 'roni', 'Bansari Kepek wonosari', 'pns', '083842161224', 70, 75, 80, 80, 'public/O59iNWhaTAg9PWZYu95blYDEDIaZNThzKojsJoJw.pdf', '1', 'public/136jOtBGAxNQCwztO5de5ll0CFrBJtX7soKBarFh.jpg', 'public/efjgMY0lIJtU5nkOaD92KLx5hRVM4glZZeNXgmlk.png', 'public/wKqO7nlKUbFR1ZUsfLDE9YseNXlYZDunROrGgpQx.png', '2023-08-24 07:48:09', '2023-08-24 07:48:09'),
+(2, 'PPDB23-01', 'Jojon', '1122334455667788', '224455', 'Bansari Kepek wonosari', '083842161224', 'gunungkidul', '2005-11-13', 'laki-laki', 'islam', 'smp 4 wno', 'Bansari Kepek wonosari', 'Baba', 'Bansari Kepek wonosari', 'buru', '083842161224', 12, 12, 12, 12, 'public/hBzZtpFSO7G202iWXhucIuZ0SbOBbHKvTVfRAEhr.jpg', '3', 'public/uBH9vidW4vdlnYwVZEtFvGT1uDKaLVG74uFvUpKv.jpg', 'public/d0koeCnZGPlaAtvyNLdKnbZvQvXzO0CXc1ukb77H.jpg', 'public/lB9VFRkwJ6xpuB1HKboMSNrbaA52esEHdT8jdNcy.pdf', '2023-08-24 08:11:39', '2023-08-24 08:11:39'),
+(3, 'PPDB23-02', 'sd', '1919191919191919', '112233', 'Bansari Kepek wonosari', '083842161224', 'Gunungkidul', '2005-04-12', 'laki-laki', 'Islam', 'SMP 1 Tepus', 'Tepus', 'Parjo', 'Tepus', 'Buruh Tani', '083842161224', 55, 78, 90, 78, 'public/IZMxKTK0M7YhPz5t9GXDwi5BKskC369xPfmWdT1O.jpg', '2', 'public/K5VNNh3vzhSwJZhxNYRdm8xta5JrzvNnTdmR6rNd.png', 'public/Y8HoI8fbyXBBSeTnS50C8vL47tQZrvGNluIDwKbO.pdf', 'public/RCLzfwZnH3g7smvR3hlPfSDLeId8sLTqW87nEvJJ.pdf', '2023-08-24 17:02:08', '2023-08-24 17:02:08');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `model_informasis`
+--
+
+CREATE TABLE `model_informasis` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `nama_jurusan` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `kuota` int(11) NOT NULL,
+  `akreditasi` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `jmlkelas` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `model_informasis`
+--
+
+INSERT INTO `model_informasis` (`id`, `nama_jurusan`, `kuota`, `akreditasi`, `jmlkelas`, `created_at`, `updated_at`) VALUES
+(1, 'Teknik Kendaraan Ringan Otomatif (TKRO)', 100, 'A', 3, '2023-08-22 07:47:21', '2023-08-22 07:47:21'),
+(2, 'Administrasi Perkantoran (AP)', 100, 'A', 3, '2023-08-22 08:20:30', '2023-08-22 08:53:07'),
+(4, 'Rekayasa Perangkat Lunak (RPL)', 100, 'A', 3, '2023-08-22 09:11:36', '2023-08-22 09:11:36');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `m_jurusans`
+--
+
+CREATE TABLE `m_jurusans` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `nama_jurusan` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `m_jurusans`
+--
+
+INSERT INTO `m_jurusans` (`id`, `nama_jurusan`, `created_at`, `updated_at`) VALUES
+(1, 'Rekaya Perangkat Lunak (RPLA)', '2023-08-21 08:00:49', '2023-08-21 08:00:49'),
+(2, 'Administrasi Perkantoran (AP)', '2023-08-21 08:01:26', '2023-08-21 08:01:26'),
+(3, 'Teknik Kendaraan Ringan Otomatif (TKRO)', '2023-08-21 08:01:45', '2023-08-21 08:01:45');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `password_reset_tokens`
+--
+
+CREATE TABLE `password_reset_tokens` (
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `personal_access_tokens`
+--
+
+CREATE TABLE `personal_access_tokens` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `tokenable_type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tokenable_id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `abilities` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `last_used_at` timestamp NULL DEFAULT NULL,
+  `expires_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `role` enum('admin','kepsek') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'admin',
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `password`, `role`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'Admin', '$2y$10$DZD5OQPTxVR.WMOttYIISukRyS69vssSKLMZASDMkvI5NGKVHl/1K', 'admin', NULL, '2023-08-22 10:03:04', '2023-08-22 10:03:04'),
+(2, 'Kepala Sekolah', '$2y$10$EClm4rFRqnRVTNLUPa9uM.z0O5jcAftggoRGhZFDooiIAXwMQo3xO', 'kepsek', NULL, '2023-08-22 10:03:04', '2023-08-22 10:03:04');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
+
+--
+-- Indeks untuk tabel `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `model_form_pendaftarans`
+--
+ALTER TABLE `model_form_pendaftarans`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `nik` (`nik`);
+
+--
+-- Indeks untuk tabel `model_informasis`
+--
+ALTER TABLE `model_informasis`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `m_jurusans`
+--
+ALTER TABLE `m_jurusans`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `password_reset_tokens`
+--
+ALTER TABLE `password_reset_tokens`
+  ADD PRIMARY KEY (`email`);
+
+--
+-- Indeks untuk tabel `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
+  ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`);
+
+--
+-- Indeks untuk tabel `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_name_unique` (`name`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT untuk tabel `model_form_pendaftarans`
+--
+ALTER TABLE `model_form_pendaftarans`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `model_informasis`
+--
+ALTER TABLE `model_informasis`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT untuk tabel `m_jurusans`
+--
+ALTER TABLE `m_jurusans`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT untuk tabel `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
